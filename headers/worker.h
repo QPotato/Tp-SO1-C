4ec752716662ffae216e31c1f054ebdf168ae59a
Fede <federicobadaloni@hotmail.com>
2016-05-09 17:45:52 -0300

    Sesion y WorkerData

--- headers/worker.h
+++ headers/worker.h
@@ -3,3 +3,16 @@
 #define N_WORKERS 6
 #define MAX_NOMBRE 128
 #define MAX_ARCHIVOS 1024
+
+typedef struct
+{
+    int id;
+    mqd_t casilla;
+    SList* archivos;
+} Sesion;
+
+typedef struct
+{
+    SList* sesiones;
+    int maxIDlocal = 0;
+} WorkerData;
